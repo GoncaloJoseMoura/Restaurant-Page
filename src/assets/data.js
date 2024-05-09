63d65764985a3220d28d64886261723867b62187
GoncaloJoseMoura <43656133+GoncaloJoseMoura@users.noreply.github.com>
2024-05-09 17:03:56 +0100

    Add Font/Pictures

---
+++ src/assets/data.js
@@ -0,0 +1,62 @@
+import banoffie_pie from "./food/banoffie_pie.jpg";
+import carob from  "./food/carob_+_dulce_de_leche_cheesecake.jpg";
+import raspberry from  "./food/raspberry_&_vanilla.jpg";
+import pistachio_scoop from "./food/pistachio_scoop-brest.jpg";
+import panna_cotta from "./food/panna_cotta_&_blueberry.jpg";
+import lemon_poppyseed from "./food/lemon_poppyseed.jpg";
+import chocolate_tart from "./food/chocolate_tart.jpg";
+import chocolate_hazelnut from "./food/chocolate_hazelnut_.jpg";
+
+
+const data = [
+    {
+"title": "Banoffie Pie",
+"price": "4,9€",
+"image": banoffie_pie,
+"ingredients": "Handmade doughnut filled with a homemade banana cream..."
+    },
+    {
+"title": "Dulce de Leche",
+"price": "4,9€",
+"image": carob,
+"ingredients": "Handmade doughnut filled with a homemade banana cream..."
+    },
+    {
+"title": "Raspberry",
+"price": "6€",
+"image": raspberry,
+"ingredients": "Handmade doughnut filled with a homemade banana cream..."
+    },
+    {
+"title": "Pistachio",
+"price": "6€",
+"image": pistachio_scoop,
+"ingredients": "Handmade doughnut filled with a homemade banana cream..."
+    },
+    {
+"title": "Panna Cotta",
+"price": "4,9€",
+"image": panna_cotta,
+"ingredients": "Handmade doughnut filled with a homemade banana cream..."
+    },
+    {
+"title": "Lemon Poppyseed",
+"price": "4,9€",
+"image": lemon_poppyseed,
+"ingredients": "Handmade doughnut filled with a homemade banana cream..."
+    },
+    {
+"title": "Chocolate Tart",
+"price": "4,9€",
+"image": chocolate_tart,
+"ingredients": "Handmade doughnut filled with a homemade banana cream..."
+    },
+    {
+"title": "Chocolate Hazelnut",
+"price": "5,9€",
+"image": chocolate_hazelnut,
+"ingredients": "Handmade doughnut filled with a homemade banana cream..."
+    }
+]
+
+export default data
